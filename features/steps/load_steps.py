6e6b636bee187492c003806a7856b5a5fab932e4
Muhammad Aliyan Sohaib <108171361+aliyanissohaib@users.noreply.github.com>
2025-06-16 14:54:33 +0500

    Update load_steps.py

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -32,21 +32,24 @@ HTTP_204_NO_CONTENT = 204
 
 @given('the following products')
 def step_impl(context):
-    """ Delete all Products and load new ones """
-    #
-    # List all of the products and delete them one by one
-    #
+    """Delete all Products and load new ones from table"""
+    # delete all products first
     rest_endpoint = f"{context.base_url}/products"
-    context.resp = requests.get(rest_endpoint)
-    assert(context.resp.status_code == HTTP_200_OK)
-    for product in context.resp.json():
-        context.resp = requests.delete(f"{rest_endpoint}/{product['id']}")
-        assert(context.resp.status_code == HTTP_204_NO_CONTENT)
+    response = requests.get(rest_endpoint)
+    assert response.status_code == status.HTTP_200_OK
+    for product in response.json():
+        del_resp = requests.delete(f"{rest_endpoint}/{product['id']}")
+        assert del_resp.status_code == status.HTTP_204_NO_CONTENT
 
-    #
     # load the database with new products
-    #
     for row in context.table:
-        #
-        # ADD YOUR CODE HERE TO CREATE PRODUCTS VIA THE REST API
-        #
+        payload = {
+            "name": row['name'],
+            "description": row['description'],
+            "price": row['price'],
+            "available": row['available'].lower() in ['true', '1'],
+            "category": row['category']
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert context.resp.status_code == status.HTTP_201_CREATED
+
